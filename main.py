4744b1cf63102a34f2f8a5f85e1fefb6c59bcd80
Max <94219555+n1skill@users.noreply.github.com>
2022-02-01 14:56:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+import telebot
+import os
+
+app = Flask(__name__)
+TOKEN = '5242978371:AAEFojIbTMFx3I6lYJUqLDq7hTexkBRFjEc'
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def message_start(message):
+    bot.send_message(message.chat.id, 'Hello, user!')
+
+
+@app.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot 01-02-2022", 200
+
+
+@app.route('/')
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://myyy-bot-1.herokuapp.com/' + TOKEN)
+    return 'Python Telegram Bot', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
