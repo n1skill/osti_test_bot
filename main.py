f75e452e3576d7cce737a22834edd628e1262b1c
Max <94219555+n1skill@users.noreply.github.com>
2022-02-01 15:51:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ def message_start(message):
     bot.send_message(message.chat.id, 'Hello, user!')
 
 
+@bot.message_handler(commands=['courses'])
+def message_courses(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+
+    with open('courses.txt') as file:
+        courses = [item.split(',') for item in file]
+
+        for title, link in courses:
+            url_button = telebot.types.InlineKeyboardButton(text=title.strip(), url=link.strip())
+            keyboard.add(url_button)
+
+        bot.send_message(message.chat.id, 'List of courses', reply_markup=keyboard)
+
+
 @app.route('/' + TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
