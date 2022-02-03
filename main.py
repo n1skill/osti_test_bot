68c9d63d560ade8621a446d917a2e4d7425493be
Max <94219555+n1skill@users.noreply.github.com>
2022-02-03 23:41:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def message_start(message):
-    bot.send_message(message.chat.id, 'Hello, user!')
+    bot.send_message(message.chat.id, 'Hello, user! =)')
 
 
 @bot.message_handler(commands=['courses'])
